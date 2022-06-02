a63611f35ef7f237beea36b4efb46ae3f74519f1
John <99531640+John-thabuks@users.noreply.github.com>
2022-06-02 11:29:30 +0300

    ...

---
+++ Biginner_Python_Projects/madlibs.py
@@ -0,0 +1,11 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Being a programmer is so {adj}! It \
+raises my dope. I love to {verb1}. Each \
+day stay hydrated and {verb2} like you are \
+{famous_person}!"
+
+print(madlib)
