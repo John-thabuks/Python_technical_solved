a53683f6fe12c60f35af855c2652025bb1e46380
John <99531640+John-thabuks@users.noreply.github.com>
2022-06-02 11:49:59 +0300

    Created a guessing game

---
+++ Biginner_Python_Projects/guess_the_number.py
@@ -0,0 +1,23 @@
+# Have the computer generate a secrete number \
+#for us to guess
+#by importing random, we are able to access all \
+#its functions in our script.
+#randint() will return a number btw 1 to what \
+#guessed
+#we will get a response of its to lowe/ to high
+
+
+import random
+
+def guess_function(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print("Sorry, guess again. Too low")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high.")
+    print(f"Yay, congrats. You have guessed the number {random_number} correctly")
+
+guess_function(15)
